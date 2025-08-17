42d56450e064b919a4d609291388295e3c9f3277
BmAravena <benjaminaravena999@gmail.com>
2025-08-16 22:16:16 -0400

    Update2: tic tac toe, many functions added: verify lines, columns, winner. And bugs were fixed correctly

--- programa/main.py
+++ programa/main.py
@@ -24,12 +24,15 @@ class Signo:
 class Tablero:
     def __init__(self, tamanio=3):
         self.tamanio = tamanio
-        self.celdas =  [["-"*tamanio] for _ in range(tamanio)]
+        #self.celdas =  [["-"*tamanio] for _ in range(tamanio)]
+        self.celdas = [["-" for _ in range(tamanio)] for _ in range(tamanio)]
+
 
     def muestraTablero(self):
         for fila in self.celdas:
             print('|'+" ".join(fila)+"|")
             #print(" | "+fila+" |")
+
 
     def verificaDisponibilidad(self, x, y):
         if self.celdas[x][y] == '-':
@@ -37,32 +40,58 @@ class Tablero:
         else:
             return False
 
+
     def colocaSigno(self, x, y, signo):
         if self.verificaDisponibilidad(x,y):
             self.celdas[x][y] = signo
         else:
             print("celda ocupada")
     
+
     def verificaLineas(self, signo):
         cantidad = 0
-        contador = 0
         for linea in range(len(self.celdas)):
-            if self.celdas[linea][contador] == signo:
-                cantidad += 1
-                if cantidad == 3:
-                    return True
-            else: 
-                contador += 1
+            for columna in range(len(self.celdas)):
+                if self.celdas[linea][columna] == signo:
+                    cantidad += 1
+                    if cantidad == 3:
+                        return True
+                    
+            cantidad = 0
 
         return False
 
 
-    def verificaColumnas():
-        return 0
+    def verificaColumnas(self, signo):
+        cantidad = 0
+        for fila in range(len(self.celdas)):
+            for columna in range(len(self.celdas)):
+                if self.celdas[columna][fila] == signo:
+                    cantidad += 1
+                    if cantidad == 3:
+                        return True
+            cantidad = 0
+
+        return False
     
-    def verificaDiagonales():
-        return 0
     
+    def verificaDiagonales(self, signo):
+        if self.celdas[0][0] == signo and self.celdas[1][1] == signo and self.celdas[2][2] == signo:
+           return True
+
+        elif self.celdas[0][2] == signo and self.celdas[1][1] == signo and self.celdas[2][0] == signo:
+           return True
+        
+        return False
+        
+
+    def verificaGanador(self, signo):
+        if (self.verificaColumnas(signo) or self.verificaLineas(signo) or self.verificaDiagonales(signo)):
+            return True
+        
+        return False
+    
+
     def lleno(self):
         for fila in self.celdas:
             if fila == '-':
@@ -79,8 +108,8 @@ class Juego:
     
     def main(self, tablero):
         fLag = True
-        tablero.muestraTablero()
         while fLag:
+            tablero.muestraTablero()
             x = int(input("Ingresa posición en x: "))
             y = int(input("Ingresa posición en y: "))
             tablero.colocaSigno(x, y, self.primer_jugador.getSigno())
@@ -88,8 +117,10 @@ class Juego:
             if x == "":
                 fLag = False
 
-            if tablero.verificaLineas(self.primer_jugador.getSigno()):
-                print("Hay ganador")
+            if tablero.verificaGanador(self.primer_jugador.getSigno()):
+                print("Hay ganador!")
+                tablero.muestraTablero()
+                fLag = False
 
 
 
